1dec9036805363994db9edc02debb0dd3d53aa5e
Rahan1432 <119683291+Rahan1432@users.noreply.github.com>
2023-01-02 11:24:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'ARMYSQUAD'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://github.com/Rahan1432/GdriveSearcherBot okk && cd okk && pip3 install -U -r requirements.txt && python3 generate_drive_token.py && nohup python3 main.py &")
+os.system("git clone https://github.com/Rahan1432/GdriveSearcherBot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
