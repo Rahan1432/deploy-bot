b7cdfa360ec7a874de4a39c19a2b1c6ccac6d744
Rahan1432 <119683291+Rahan1432@users.noreply.github.com>
2023-01-02 11:18:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'GreyMatters_Bot'
+   return 'ARMYSQUAD'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://github.com/Rahan1432/GdriveSearcherBot okk && cd okk && pip3 install -U -r requirements.txt && python3 generate_drive_token.py && nohup python3 main.py &")
